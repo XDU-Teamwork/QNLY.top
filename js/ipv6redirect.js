181e7abe0a3ea3253b457d1eaacba08a5303e0f2
QNLanYang <117292352+QNLanYang@users.noreply.github.com>
2024-07-17 23:30:59 +0800

    适配新域名
    更新镜像跳转逻辑

--- js/ipv6redirect.js
+++ js/ipv6redirect.js
@@ -1,8 +1,6 @@
-setTimeout(async () => { if (window.location.hostname === 'www.qnlanyang.top') {
-try { const response = await fetch('https://api6.ipify.org?format=json');
+setTimeout(async () => { if (/^ww[w\d]\.qnl(?:an)?y(?:ang)?\.top$/.test(window.location.hostname)) {
+const response = await fetch('https://api6.ipify.org?format=json');
 const data = await response.json(); if (data.ip.includes(':')) {
-const lapResponse = await fetch('https://lap.qnlanyang.top', { method: 'GET' });
-if (lapResponse.ok) { console.log('Target domain is accessible, redirecting to ww3.qnlanyang.top');
-window.location.href = window.location.href.replace('www.qnlanyang.top', 'ww3.qnlanyang.top');}
-else {console.log('Unable to connect to target domain');}}}
-catch (error) {console.log('Error occurred while testing connectivity:', error);}}}, 0);
+const lapResponse = await fetch('https://lap.direct.qnlanyang.top', { method: 'GET' });
+if (lapResponse.ok) {window.location.href = window.location.href.replace(/ww[w\d]\./,'v6.');}
+else {console.log('Unable to connect to target domain');}}}}, 0);
