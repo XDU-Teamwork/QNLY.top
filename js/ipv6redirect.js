7663b08a517d98ba472d5793833dd9edba614639
QNLanYang <117292352+QNLanYang@users.noreply.github.com>
2024-07-25 19:30:17 +0800

    优化正则表达式
    匹配更精准
    去掉查询当前IP请求（测试，如果能直接访问说明有v6）

--- js/ipv6redirect.js
+++ js/ipv6redirect.js
@@ -1,6 +1,6 @@
-setTimeout(async () => { if (/^ww[w\d]\.qnl(?:an)?y(?:ang)?\.top$/.test(window.location.hostname)) {
-const response = await fetch('https://api6.ipify.org?format=json');
-const data = await response.json(); if (data.ip.includes(':')) {
+setTimeout(async () => { if (/^ww[w\d]\.qnl(y|anyang)\.top$/.test(window.location.hostname)) {
+//const response = await fetch('https://api6.ipify.org?format=json');
+//const data = await response.json(); if (data.ip.includes(':')) {
 const lapResponse = await fetch('https://lap.direct.qnlanyang.top', { method: 'GET' });
 if (lapResponse.ok) {window.location.href = window.location.href.replace(/ww[w\d]\./,'v6.');}
-else {console.log('Unable to connect to target domain');}}}}, 0);
+else {console.log('Unable to connect to target domain');}}}/*}*/, 0);
